37ba402a67487a985f65cf58f5aae9ab1cd93c49
Ulf Sauer <ulf.sauer@gmail.com>
2024-10-16 14:29:22 +0200

    Show "last updated" on page

--- ui/index.py
+++ ui/index.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 from media.item import Item
@@ -19,5 +21,6 @@ def render_index(env: Environment, items: list[Item]) -> str:
                     available_items_by_location[location] = []
                 available_items_by_location[location].append(item)
 
+    current_time = datetime.now().strftime("%d.%m.%Y %H:%M")
     template = env.get_template("index.j2")
-    return template.render(location_mapping=available_items_by_location)
+    return template.render(location_mapping=available_items_by_location, current_time=current_time)
