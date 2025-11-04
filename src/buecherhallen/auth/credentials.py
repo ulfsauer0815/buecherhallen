a9b1f3ad32a63fecf1e1aa6c30c17f368889e2bd
Ulf Sauer <ulf.sauer@gmail.com>
2025-11-04 18:15:57 +0100

    Check if required env vars are set

--- src/buecherhallen/auth/credentials.py
+++ src/buecherhallen/auth/credentials.py
@@ -8,6 +8,13 @@ class Credentials:
 
 
 def retrieve_credentials() -> Credentials:
-    username = os.getenv('BH_USERNAME')
-    password = os.getenv('BH_PASSWORD')
+    username = __get_required_env('BH_USERNAME')
+    password = __get_required_env('BH_PASSWORD')
     return Credentials(username, password)
+
+
+def __get_required_env(env_name: str) -> str:
+    value = os.getenv(env_name)
+    if value is None:
+        raise ValueError(f"Environment variable {env_name} is required but not set")
+    return value
