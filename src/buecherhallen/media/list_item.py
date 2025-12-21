2392db9abf293f50ae2e053ef42a8a1389242b07
Ulf Sauer <ulf.sauer@gmail.com>
2025-12-22 00:57:08 +0100

    Support other sources and digital media (#15)
    Assume source is ILS if not present

--- src/buecherhallen/media/list_item.py
+++ src/buecherhallen/media/list_item.py
@@ -1,19 +1,26 @@
 from typing import Any, Optional
 
+from buecherhallen.common.constants import BASE_URL
+
 
 class ListItem:
-    def __init__(self, item_id: str, title: str, author: Optional[str]):
+    def __init__(self, item_id: str, source: str, title: str, author: Optional[str]):
         self.item_id = item_id
+        self.source = source
         self.title = title
         self.author = author
 
     def __repr__(self):
         return f"ListItem({self.item_id}, {self.title}, {self.author})"
 
+    def get_url(self) -> str:
+        return f"{BASE_URL}/manifestations/{self.item_id}?source={self.source}"
+
     @staticmethod
     def from_json(raw: dict[str, Any]) -> 'ListItem':
         item_id = raw.get("id")
         assert item_id is not None
+        source = raw.get("source", "ILS")
         title = None
         author = None
 
@@ -28,4 +35,4 @@ class ListItem:
 
         assert title is not None
 
-        return ListItem(item_id, title, author)
+        return ListItem(item_id, source, title, author)
